515e3e01e34a7608094bf793ca4f731d5793c17c
Karik Isichei <karik.isichei@gmail.com>
2020-11-16 15:56:36 +0000

    Moving stuff around

--- arrow_pd_parser/pa_pd.py
+++ arrow_pd_parser/pa_pd.py
@@ -2,10 +2,11 @@ import pyarrow as pa
 import pandas as pd
 
 
-def generate_type_mapper(new_integer, new_string):
+def generate_type_mapper(pd_boolean, pd_integer, pd_string):
     tm = {}
-    if new_integer:
-        int_map = {pa.bool_(): pd.BooleanDtype()}
+    if pd_boolean:
+        bool_map = {pa.bool_(): pd.BooleanDtype()}
+        tm = {**tm, **bool_map}
 
     if tm:
         return tm.get
@@ -13,7 +14,14 @@ def generate_type_mapper(new_integer, new_string):
         return None
 
 
-def arrow_to_pd(arrow_table, new_integer=True, new_string=True, date_type: str, datetime_type: str):
+def arrow_to_pandas(
+    arrow_table,
+    pd_boolean=True,
+    pd_integer=True,
+    pd_string=True,
+    date_type: str = None,
+    datetime_type: str = None,
+):
     """Converts arrow table to stricter pandas datatypes based on options.
 
     Args:
@@ -27,6 +35,5 @@ def arrow_to_pd(arrow_table, new_integer=True, new_string=True, date_type: str, 
     Returns:
         [type]: [description]
     """
-    tm = generate_type_mapper(new_integer, new_string)
-    return arrow_table.to_pandas(types_mapper=tm)
-
+    tm = generate_type_mapper(pd_boolean, pd_integer, pd_string)
+    return arrow_table.to_pandas(types_mapper=tm, datetime_as_object=True)
---
+++ arrow_pd_parser/parse.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pyarrow as pa
+from pyarrow import csv
+
+from arrow_pd_parser.pa_pd import arrow_to_pandas
+
+
+def pa_read_csv(csv_path, test_col_types):
+    csv_co = csv.ConvertOptions(column_types=test_col_types)
+    pa_csv_table = csv.read_csv(csv_path, convert_options=csv_co)
+    return pa_csv_table
+
+
+def pa_read_csv_to_pandas(csv_path, test_col_types, new_int_type: bool = True):
+    arrow_table = pa_read_csv(csv_path, test_col_types)
+    return arrow_to_pandas(arrow_table, new_int_type=new_int_type)
