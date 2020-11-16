b66cd87e50bcad62bd58f15b1bc4afdd093a14e6
Alec Johnson <alec.johnson@digital.justice.gov.uk>
2020-11-16 17:42:19 +0000

    Adjusted to fit new structure

--- arrow_pd_parser/pa_pd.py
+++ arrow_pd_parser/pa_pd.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pyarrow as pa
 import pandas as pd
 
@@ -7,6 +8,31 @@ def generate_type_mapper(pd_boolean, pd_integer, pd_string):
     if pd_boolean:
         bool_map = {pa.bool_(): pd.BooleanDtype()}
         tm = {**tm, **bool_map}
+
+    if pd_integer:
+        int_map = {
+            pa.int8(): pd.Int64Dtype(),
+            pa.int16(): pd.Int64Dtype(),
+            pa.int32(): pd.Int64Dtype(),
+            pa.int64(): pd.Int64Dtype(),
+            pa.uint8(): pd.Int64Dtype(),
+            pa.uint16(): pd.Int64Dtype(),
+            pa.uint32(): pd.Int64Dtype(),
+            pa.uint64(): pd.Int64Dtype(),
+        }
+        tm = {**tm, **int_map}
+    else:
+        int_map = {
+            pa.int8: np.float64,
+            pa.int16: np.float64,
+            pa.int32: np.float64,
+            pa.int64: np.float64,
+            pa.uint8: np.float64,
+            pa.uint16: np.float64,
+            pa.uint32: np.float64,
+            pa.uint64: np.float64,
+        }
+        tm = {**tm, **int_map}
 
     if tm:
         return tm.get
@@ -29,11 +55,11 @@ def arrow_to_pandas(
 
         new_integer (bool, optional): converts bools to the new pandas BooleanDtype.
         Otherwise will convert to bool (if not nullable) and object of (True, False, None) if nulls exist. Defaults to True.
-        
+
         new_string (bool, optional): [description]. Defaults to True.
 
     Returns:
         [type]: [description]
     """
     tm = generate_type_mapper(pd_boolean, pd_integer, pd_string)
-    return arrow_table.to_pandas(types_mapper=tm, datetime_as_object=True)
+    return arrow_table.to_pandas(types_mapper=tm)
--- arrow_pd_parser/parse.py
+++ arrow_pd_parser/parse.py
@@ -1,7 +1,6 @@
-import numpy as np
-import pandas as pd
-import pyarrow as pa
 from pyarrow import csv
+
+from arrow_pd_parser.pa_pd import arrow_to_pandas
 
 
 def pa_read_csv(csv_path, test_col_types):
@@ -10,38 +9,6 @@ def pa_read_csv(csv_path, test_col_types):
     return pa_csv_table
 
 
-def pa_to_pd(arrow_table, pd_integer: bool = True):
-    if pd_integer:
-        int_mapper = {
-            pa.int8(): pd.Int64Dtype(),
-            pa.int16(): pd.Int64Dtype(),
-            pa.int32(): pd.Int64Dtype(),
-            pa.int64(): pd.Int64Dtype(),
-            pa.uint8(): pd.Int64Dtype(),
-            pa.uint16(): pd.Int64Dtype(),
-            pa.uint32(): pd.Int64Dtype(),
-            pa.uint64(): pd.Int64Dtype(),
-        }
-        return arrow_table.to_pandas(
-            timestamp_as_object=True, types_mapper=int_mapper.get
-        )
-
-    else:
-        int_mapper = {
-            pa.int8: np.float64,
-            pa.int16: np.float64,
-            pa.int32: np.float64,
-            pa.int64: np.float64,
-            pa.uint8: np.float64,
-            pa.uint16: np.float64,
-            pa.uint32: np.float64,
-            pa.uint64: np.float64,
-        }
-        return arrow_table.to_pandas(
-            timestamp_as_object=True, types_mapper=int_mapper.get
-        )
-
-
 def pa_read_csv_to_pandas(csv_path, test_col_types, pd_integer: bool = True):
     arrow_table = pa_read_csv(csv_path, test_col_types)
-    return pa_to_pd(arrow_table, pd_integer=pd_integer)
+    return arrow_to_pandas(arrow_table, pd_integer=pd_integer)
