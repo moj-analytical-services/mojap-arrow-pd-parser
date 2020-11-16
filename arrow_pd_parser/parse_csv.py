cbc457104882a64ca2e7a36f06c31a6103b30e60
Alec Johnson <alec.johnson@digital.justice.gov.uk>
2020-11-16 15:15:12 +0000

    Slightly reordered file

--- arrow_pd_parser/parse_csv.py
+++ arrow_pd_parser/parse_csv.py
@@ -4,11 +4,6 @@ import pyarrow as pa
 from pyarrow import csv
 
 
-def pa_read_csv(csv_path, test_col_types):
-    csv_co = csv.ConvertOptions(column_types=test_col_types)
-    pa_csv_table = csv.read_csv(csv_path, convert_options=csv_co)
-    return pa_csv_table
-
 
 int_types = [
     pa.int8(),
@@ -20,6 +15,12 @@ int_types = [
     pa.uint32(),
     pa.uint64(),
 ]
+
+
+def pa_read_csv(csv_path, test_col_types):
+    csv_co = csv.ConvertOptions(column_types=test_col_types)
+    pa_csv_table = csv.read_csv(csv_path, convert_options=csv_co)
+    return pa_csv_table
 
 
 def int64_mapper(data_type):
@@ -38,9 +39,7 @@ def pa_to_pd(arrow_table, new_int_type: bool = True):
             timestamp_as_object=True, types_mapper=int64_mapper
         )
     else:
-        return arrow_table.to_pandas(
-            timestamp_as_object=True
-        )
+        return arrow_table.to_pandas(timestamp_as_object=True)
 
 
 def pa_read_csv_to_pandas(csv_path, test_col_types, new_int_type: bool = True):
