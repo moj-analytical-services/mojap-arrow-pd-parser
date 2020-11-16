5d4316b277ddbd7d257b9424eab5d30d6e35366e
green-digger <jacob.browning@digital.justice.gov.uk>
2020-11-16 17:11:55 +0000

    Conversion of string type with passing tests successfully

--- tests/test_str_conformance.py
+++ tests/test_str_conformance.py
@@ -1,21 +1,40 @@
 import pytest
 import pyarrow as pa
+from pyarrow import csv
+import pandas as pd
 
-from arrow_pd_parser.parse_csv import pa_read_csv_to_pandas
+from arrow_pd_parser.parse import pa_read_csv_to_pandas
+
+
+def convert_a_to_pd_convert():
+    csv_to_pyarrow = csv.read_csv(input_file="tests/data/string_type.csv")
+    # print(csv_to_pyarrow.schema)
+    tm = {pa.string(): pd.StringDtype()}
+    pyarrow_to_pandas = csv_to_pyarrow.to_pandas(types_mapper=tm.get)
+    return pyarrow_to_pandas
+
+
+def convert_a_to_pd():
+    csv_to_pyarrow = csv.read_csv(input_file="tests/data/string_type.csv")
+    # print(csv_to_pyarrow.schema)
+    pyarrow_to_pandas = csv_to_pyarrow.to_pandas()
+    return pyarrow_to_pandas
 
 
 @pytest.mark.parametrize(
     "in_type,pd_old_type,pd_new_type",
     [("string", "object", "string")],
 )
-def test_int(in_type, pd_old_type, pd_new_type):
-    test_col_types = {"int_col": getattr(pa, in_type)()}
+def test_string(in_type, pd_old_type, pd_new_type):
+    test_col_types = {"string_col": getattr(pa, "string")()}
     df_old = pa_read_csv_to_pandas(
-        "tests/data/all_types.csv", test_col_types, new_int_type=False
+        "tests/data/string_type.csv", test_col_types, pd_string=False
     )
-    assert str(df_old.my_int.dtype) == pd_old_type
+    # df_old = convert_a_to_pd()
+    assert str(df_old.my_string.dtype) == pd_old_type
 
     df_new = pa_read_csv_to_pandas(
-        "tests/data/all_types.csv", test_col_types, new_int_type=True
+        "tests/data/string_type.csv", test_col_types, pd_string=True
     )
-    assert str(df_new.my_int.dtype) == pd_new_type
+    # df_new = convert_a_to_pd_convert()
+    assert str(df_new.my_string.dtype) == pd_new_type
