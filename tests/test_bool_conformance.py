1752d8d9440553058e6cb55504f049bd5b33e4ae
thomas-hirsch <thomas.hirsch24@gmail.com>
2020-11-17 10:20:38 +0000

    fix boolean tests

--- tests/test_bool_conformance.py
+++ tests/test_bool_conformance.py
@@ -11,11 +11,11 @@ from arrow_pd_parser.parse import pa_read_csv_to_pandas
 def test_bool(col_name, pd_old_type, pd_new_type):
     test_col_types = {"bool_col": getattr(pa, "bool_")()}
     df_old = pa_read_csv_to_pandas(
-        "tests/data/bool_type.csv", test_col_types, new_bool_type=False
+        "tests/data/bool_type.csv", test_col_types, pd_boolean=False
     )
     assert str(df_old[col_name].dtype) == pd_old_type
 
     df_new = pa_read_csv_to_pandas(
-        "tests/data/bool_type.csv", test_col_types, new_bool_type=True
+        "tests/data/bool_type.csv", test_col_types, pd_boolean=True
     )
     assert str(df_new[col_name].dtype) == pd_new_type
