05bf3049dec95717d8a735e926b507ffd5c84efe
Alec Johnson <alec.johnson@digital.justice.gov.uk>
2020-11-17 12:25:31 +0000

    Test data set up - now for actual assertions

--- tests/test_str_conformance.py
+++ tests/test_str_conformance.py
@@ -5,8 +5,7 @@ from arrow_pd_parser.parse import pa_read_csv_to_pandas
 
 
 @pytest.mark.parametrize(
-    "in_type,pd_old_type,pd_new_type",
-    [("string", "object", "string")],
+    "in_type,pd_old_type,pd_new_type", [("string", "object", "string")],
 )
 def test_string(in_type, pd_old_type, pd_new_type):
     test_col_types = {"string_col": getattr(pa, "string")()}
@@ -22,18 +21,30 @@ def test_string(in_type, pd_old_type, pd_new_type):
 
 
 @pytest.mark.parametrize(
-    "in_type,pd_old_type,pd_new_type",
-    [("string", "object", "string")],
+    "in_type,pd_old_type,pd_new_type", [("string", "object", "string")],
 )
 def test_csv_options(in_type, pd_old_type, pd_new_type):
-    parse_options = {"quote_char": "'", "escape_char": "\\"}
     test_col_types = {"string_col": getattr(pa, "string")()}
-    df_old = pa_read_csv_to_pandas(
-        "tests/data/csv_options_test.csv", test_col_types, pd_string=False, parse_options=parse_options
-    )
-    assert str(df_old.my_string.dtype) == pd_old_type
+    parse_options = {
+        "quote_char": "'",
+        "escape_char": "\\",
+        "delimiter": ";",
+        "newlines_in_values": True,
+    }
+    read_options = {"skip_rows": 1}
+    convert_options = {
+        "include_columns": ["i", "my_string", "nonexistent_column"],
+        "include_missing_columns": True,
+        "null_values": ["NULL_STRING"],
+    }
 
-    df_new = pa_read_csv_to_pandas(
-        "tests/data/csv_options_test.csv", test_col_types, pd_string=True, parse_options=parse_options
+    df = pa_read_csv_to_pandas(
+        "tests/data/csv_options_test.csv",
+        test_col_types,
+        pd_string=False,
+        parse_options=parse_options,
+        convert_options=convert_options,
+        read_options=read_options,
     )
-    assert str(df_new.my_string.dtype) == pd_new_type
+    print(df.columns.tolist())
+    assert df.columns.tolist() == ["i", "my_string", "nonexistent_column"]
