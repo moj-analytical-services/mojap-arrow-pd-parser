14c35dd516df3b3338d3bd16f3e3ecbeeb654a79
GeorgeKelly <george.kelly@digital.justice.gov.uk>
2020-11-17 16:40:55 +0000

    csv and json the same test

--- tests/test_bool_conformance.py
+++ tests/test_bool_conformance.py
@@ -31,3 +31,12 @@ def test_bool_jsonl(col_name, pd_old_type, pd_new_type):
 
     df_new = pa_read_json_to_pandas("tests/data/bool_type.jsonl", pd_boolean=True)
     assert str(df_new[col_name].dtype) == pd_new_type
+
+
+def test_bool_csv_and_json():
+    test_col_types = {"bool_col": getattr(pa, "bool_")()}
+    df_csv = pa_read_csv_to_pandas(
+        "tests/data/bool_type.csv", test_col_types, pd_boolean=True
+    )
+    df_jsonl = pa_read_json_to_pandas("tests/data/bool_type.jsonl", pd_boolean=True)
+    assert df_csv.equals(df_jsonl)
