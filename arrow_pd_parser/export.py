854f8d868de14f514e761e93b945d823bc2e42ef
s-ducks <stephen.bias@digital.justice.gov.uk>
2021-04-09 11:45:55 +0100

    added export to parquet

--- arrow_pd_parser/export.py
+++ arrow_pd_parser/export.py
@@ -1,6 +1,7 @@
 import datetime
 import numpy as np
 import pandas as pd
+import pyarrow as pa
 
 from typing import Union, IO
 
@@ -67,3 +68,33 @@ def pd_to_json(
             new[col].replace("NaT", np.nan, regex=False, inplace=True)
 
     new.to_json(output_file, orient=orient, lines=lines, **kwargs)
+
+
+def pd_to_parquet(
+    df: pd.DataFrame,
+    output_file: Union[IO, str],
+    arrow_schema: pa.lib.Schema,
+):
+    """
+    Export a data frame as parquet
+    Does no conversion, and adheres to the provided schema
+
+    Args:
+        df (pd.DataFrame): a pandas dataframe
+        output_file (IO or str): the path you want to export to
+    """
+    if not isinstance(arrow_schema, pa.lib.schema):
+        raise TypeError(
+            f"arrow schema must be pyarrow schema, found {type(arrow_schema)}"
+        )
+    table = pa.Table.from_pandas(df)
+    table = table.cast(arrow_schema)
+
+    if isinstance(output_file, str):
+        s3 = pa.fs.S3FileSystem(region='eu-west-1')
+        with s3.open_output_stream(output_file.replace("s3://","")) as f:
+            pa.parquet.write_table(table, f)
+    elif isinstance(output_file, IO):
+        pa.parquet.write_table(table, output_file)
+    else:
+        raise TypeError(f"output file must be IO or str, found {type(output_file)}")
